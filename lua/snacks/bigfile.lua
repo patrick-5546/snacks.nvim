c49c3f364cde3bb1bbf79e501d1492ea8e2397bc
Diogo Pombeiro <162458559+Necrom4@users.noreply.github.com>
2025-11-09 10:43:56 +0100

    perf(bigfile): disable completion to avoid lag when entering insert mode (#2475)
    ## Description
    
    Default LazyVim uses the extra
    [Blink.cmp](https://github.com/saghen/blink.cmp) which creates lag
    (multiple seconds in a 600MB `csv` test file) when entering insert mode.

--- lua/snacks/bigfile.lua
+++ lua/snacks/bigfile.lua
@@ -20,6 +20,7 @@ local defaults = {
       vim.cmd([[NoMatchParen]])
     end
     Snacks.util.wo(0, { foldmethod = "manual", statuscolumn = "", conceallevel = 0 })
+    vim.b.completion = false
     vim.b.minianimate_disable = true
     vim.b.minihipatterns_disable = true
     vim.schedule(function()
